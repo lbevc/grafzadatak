873887c6f27b6d3445810566f1bbae8dea034147
Lana Bevc <bevc.lana@gmail.com>
2023-03-11 16:09:08 +0100

    Add files via upload

---
+++ WpfApp1/WpfApp1/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace WpfApp1
+{
+    public class Class2
+    {
+        public ISeries[] Series { get; set; }= new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null
+                }
+            };
+    }
+}
